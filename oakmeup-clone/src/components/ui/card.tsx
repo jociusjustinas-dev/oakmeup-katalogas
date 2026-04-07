aa67499a492f4a401b14f8c5702881b8efa2bb31
Justinas Jocius <jociusjustinas@gmail.com>
2026-04-07 15:20:13 +0300

    Revert "Merge branch 'chore/sku-artifacts-and-deps'"
    This reverts commit 09ea5d2477b86f48f1bda1d9ed621a841a3d96d8, reversing
    changes made to 6890db28625fae013366c29c3003e80d7a25f920.

--- oakmeup-clone/src/components/ui/card.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
-
-function Card({ className, ...props }: React.ComponentProps<"div">) {
-  return <div className={cn("rounded-xl border border-zinc-200 bg-white", className)} {...props} />;
-}
-
-export { Card };
